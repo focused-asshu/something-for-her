512bb1038fdeb7722648fbb805765da037a41772
Aashish Mishra <focused.aashish@gmail.com>
2026-06-29 21:24:59 +0530

    Build cinematic confession website

---
+++ tailwind.config.js
@@ -0,0 +1,1 @@
+export default { content: ['./index.html','./src/**/*.{js,jsx}'], theme: { extend: { colors: { space: '#0a0a0f', rose: '#e8678a', purple: '#9b7fe8', warm: '#f5f0ff' }, fontFamily: { serif: ['Cormorant Garamond','serif'], sans: ['Inter','sans-serif'] } } }, plugins: [] };
